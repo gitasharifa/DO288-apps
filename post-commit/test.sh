fc4234819471b87d0bc05e7a386c1ce95b8fff09
Student User <student@workstation.lab.example.com>
2021-07-20 23:02:00 -0400

    Post commit 2

--- post-commit/test.sh
+++ post-commit/test.sh
@@ -1,1 +1,2 @@
-curl -s -S -i -X POST http://builds-for-managers--post-commit./api/builds -f -d 'developer=${DEVELOPER}&git=${OPENSHIFT_BUILD_SOURCE}&project=${OPENSHIFT_BUILD_NAMESPACE}'
+#!/bin/bash
+"\"curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
